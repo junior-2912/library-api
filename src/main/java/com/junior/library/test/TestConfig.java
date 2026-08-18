5f603dc3f2327121ec68f3f7ce0a5d98609cc950
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-08-18 12:44:00 -0300

    feat: users test

--- src/main/java/com/junior/library/test/TestConfig.java
+++ src/main/java/com/junior/library/test/TestConfig.java
@@ -1,8 +1,10 @@
 package com.junior.library.test;
 
 import com.junior.library.entitie.Book;
+import com.junior.library.entitie.User;
 import com.junior.library.enums.BookStatus;
 import com.junior.library.repository.BookRepository;
+import com.junior.library.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +17,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private BookRepository bookRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public void run(String... args) throws Exception {
         Book b1 = new Book(null, "Código Limpo", "Robert C. Martin", BookStatus.AVAILABLE);
@@ -23,6 +28,13 @@ public class TestConfig implements CommandLineRunner {
         Book b4 = new Book(null, "O Codificador Limpo", "Robert C. Martin", BookStatus.AVAILABLE);
         Book b5 = new Book(null, "Introdução à Programação com Python", "Nilo Ney Coutinho Menezes", BookStatus.AVAILABLE);
 
+        User u1 = new User(null, "Jhon Carter", null);
+        User u2 = new User(null, "Martin Storm", null);
+        User u3 = new User(null, "Jason Jackson", null);
+        User u4 = new User(null, "Mariah Robert", null);
+        User u5 = new User(null, "Will Clawer", null);
+
         bookRepository.saveAll(Arrays.asList(b1, b2, b3, b4, b5));
+        userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5));
     }
 }
