1782c5d687d030799df310ce163f860bb2f77fa6
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-08-18 12:43:33 -0300

    feat: implements findById

--- src/main/java/com/junior/library/service/UserService.java
+++ src/main/java/com/junior/library/service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,5 +21,9 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new NoSuchElementException("User not found!"));
     }
 }
