5df753befb1480b46e9ac6697cdfc3909fc49534
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-08-31 16:32:43 -0300

    feat: add new exceptions

--- src/main/java/com/junior/library/exceptions/ResourceNotFoudException.java
+++
@@ -1,7 +0,0 @@
-package com.junior.library.exceptions;
-
-public class ResourceNotFoudException extends RuntimeException {
-    public ResourceNotFoudException(String message) {
-        super(message);
-    }
-}
