69e931a4095814c645b4d7a3d9b4b8e286f8fd92
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-08-31 20:01:35 -0300

    feat: add getters and setters

--- src/main/java/com/junior/library/entities/Book.java
+++ src/main/java/com/junior/library/entities/Book.java
@@ -59,6 +59,14 @@ public class Book implements Serializable {
         return isbn;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
