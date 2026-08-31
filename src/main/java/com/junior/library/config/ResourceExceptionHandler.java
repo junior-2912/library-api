3563250a8fc99edc87e94e00ce54319ea50b2aaa
Genilson Junior <genilsonjuniordossantos33@gmail.com>
2026-08-31 16:33:00 -0300

    feat: implements handler exception

--- src/main/java/com/junior/library/config/ResourceExceptionHandler.java
+++ src/main/java/com/junior/library/config/ResourceExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.junior.library.config;
 
 import com.junior.library.exceptions.BookIsNotAvailableException;
+import com.junior.library.exceptions.LoanAlreadyFinishedException;
+import com.junior.library.exceptions.ResourceNotFoundException;
+import com.junior.library.exceptions.UserLoanLimitExceededException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +18,52 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> bookIsNotAvailable(BookIsNotAvailableException e, HttpServletRequest request) {
         StandardError error = new StandardError();
 
-        error.setInstant(Instant.now());
+        error.setTimestamp(Instant.now());
         error.setStatus(HttpStatus.CONFLICT.value());
         error.setError("Conflict");
         error.setMessage(e.getMessage());
         error.setPath(request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
-
     }
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        StandardError error = new StandardError();
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setError("Not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+
+    @ExceptionHandler(LoanAlreadyFinishedException.class)
+    public ResponseEntity<StandardError> loanAlreadyFinished(LoanAlreadyFinishedException e, HttpServletRequest request) {
+        StandardError error = new StandardError();
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.CONFLICT.value());
+        error.setError("Conflict");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    @ExceptionHandler(UserLoanLimitExceededException.class)
+    public ResponseEntity<StandardError> userLoanLimitExceeded(UserLoanLimitExceededException e, HttpServletRequest request) {
+        StandardError error = new StandardError();
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.CONFLICT.value());
+        error.setError("Conflict");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
 }
